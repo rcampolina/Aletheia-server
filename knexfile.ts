205af2b10cf39c81c784393cc605f54348699fe9
Rafael Campolina <rafaelcampolina@outlook.com.br>
2021-09-11 20:18:05 -0300

    Configurando variável de ambiente no migration

--- knexfile.ts
+++ knexfile.ts
@@ -2,15 +2,9 @@ import path from 'path';
 
 module.exports = {
   client: 'postgresql',
-  connection: {
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: '1816',
-    database: 'Aletheia',
-  },
+  connection: process.env.DB_URI,
+  useNullAsDefault: true,
   migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations')
   },
-  useNullAsDefault: true,
 };
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,13 +2,7 @@ import knex from 'knex';
 
 const db = knex({
   client: 'postgresql',
-  connection: {
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: '1816',
-    database: 'Aletheia',
-  },
+  connection: process.env.DB_URI,
   useNullAsDefault: true,
 });
 
