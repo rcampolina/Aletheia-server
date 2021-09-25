755fad689fa09080818b186f6952e4c95d83a4b8
Rafael Campolina <rafaelcampolina@outlook.com.br>
2021-09-25 20:41:57 -0300

    Alterando o nome da variavel de ambiente do Sentry

--- src/services/sentryConfig.ts
+++ src/services/sentryConfig.ts
@@ -2,7 +2,7 @@ import * as Sentry from "@sentry/node";
 
 export default async function SentryConfig() {
   Sentry.init({
-    dsn: process.env.SENTRY_CONFIG,
+    dsn: process.env.SENTRY_SERVER,
     tracesSampleRate: 1.0,
   });
 }
